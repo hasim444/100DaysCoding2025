34501801a3d98b135c736cde7df1e3a0c80c344e
muh. hasyim <h13880134@gmail.com>
2025-09-10 16:04:54 +0800

    Day3.java

---
+++ Day3.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        
+    // println (digunakan untuk mencetak teks atau nilai ke layar dan otomatis pindah ke baris baru setelahnya)
+        System.out.println("Nama Saya: Muh.hasyim");
+        System.out.println("Prodi    : Informatika");
+        System.out.println("Umur saya: 18 tahun ");
+        System.out.println("alamat   : malunda");
+        
+       // print(digunakan untuk mencetak teks atau nilai ke layar tanpa menambah baris baru.)
+        System.out.print("Nama Muh.hasyim");
+        System.out.print(", Prodi Informatika");
+        System.out.print(", Umur saya 18 tahun");
+        System.out.println(",alamat malunda");
+        
+        // printf(dipakai untuk mencetak teks dengan format tertentu)
+        System.out.printf("\nNama saya %s %nUmur %d %nProdi Informatika  ","muh.hasyim",18,"Informatika");
+        System.out.println();
+        
+    }
+    
+}
