2cb066411e2376303628dfebb2b7e552c1861359
muh. hasyim <h13880134@gmail.com>
2025-09-21 20:48:22 +0800

    Day6.java

---
+++ Day6.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        //Menyimpan bilangan pecahan
+        double tinggi = 170.5;
+        float kg = 63.5f;
+        
+        // cetak (hasil bilangan pecahan)
+        System.out.printf("Tinggi badan : %.1f cm%n", tinggi);
+        System.out.printf("Berat badan  : %.1f kg%n", kg);
+    }
+}
