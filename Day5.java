1588da9e876289f68d288cbeb11d6d391a9de04c
muh. hasyim <h13880134@gmail.com>
2025-09-21 20:47:35 +0800

    Day5.java

---
+++ Day5.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        //tipe data (bilangan bulat)
+        byte angka1 = 127;
+        short angka2 = 32767;
+        int angka3 = 203155805;
+        long angka4 = 1000000000;
+        
+       // printf untuk merapikan (dengan karakter)
+        System.out.printf("byte  : %d%n", angka1);
+        System.out.printf("short : %d%n", angka2);
+        System.out.printf("int   : %d%n", angka3);
+        System.out.printf("long  : %d%n", angka4);
+    }
+}
