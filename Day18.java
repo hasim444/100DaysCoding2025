1759a7a9cbdafc28380a6941908acfc24fe13d04
muh. hasyim <h13880134@gmail.com>
2025-09-25 22:17:48 +0800

    Day18.java

---
+++ Day18.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class AritKontol {
+    
+    public static void main(String[] args) {
+        Scanner isc = new Scanner (System.in);
+        
+   System.out.println("<====(tipe data kecil ke besar)====>");
+        byte angka1 = isc.nextByte();
+        System.out.println("1.byte  \t="+angka1);
+        short angka2 = angka1;
+        System.out.println("2.short \t="+angka2);
+        int angka3 = angka2;
+        System.out.println("3.int   \t="+angka3);
+        long angka4 = angka3;
+        System.out.println("4.long  \t="+angka4);
+        float angka5 = angka4;
+        System.out.println("5.float \t="+angka5);
+        double angka6 = angka5;
+        System.out.println("6.double\t="+angka6);
+        System.out.println("<==================================>");
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
+    }
+    
+}
