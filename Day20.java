b16d40ee58a9b7ebfd05273297ab94727023d86d
muh. hasyim <h13880134@gmail.com>
2025-09-27 23:03:08 +0800

    Day20.java

---
+++ Day20.java
@@ -0,0 +1,14 @@
+class Main {
+    public static void main(String[] args) {
+
+        // int → String
+        int angka = 123;
+        String angka1 = Integer.toString(angka);
+        System.out.println("Ttipe data int ke String   : " + angka1);
+      // double → String
+        double angka2 = 170.5;
+        String angka3 = Double.toString(angka2);
+        System.out.println("Tipe data double ke String : " + angka3);
+    }
+}
+
