7e10547ada1a97ceab55ca11a8f9e56c7c3c021a
muh. hasyim <h13880134@gmail.com>
2025-09-29 23:37:23 +0800

    Day22.java

---
+++ Day22.java
@@ -0,0 +1,20 @@
+package javaday22n;
+import java.util.Scanner;
+public class JavaDay22n {
+    
+public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    
+    int L,s;
+    System.out.print("L : ");
+    s = input.nextInt();
+    System.out.print("s : ");
+    s = input.nextInt();
+    L = 5;
+    int hasil;
+    hasil = s*s;
+    System.out.println("keliling hasil : " + hasil+" cm");
+    
+   
+    }
+}
