62fc60b28bb2ea5cce591bdc006898605a6347a9
muh. hasyim <h13880134@gmail.com>
2025-10-02 23:24:50 +0800

    Day25.java

---
+++ Day25.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class D0225303 {
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a,b;
+        System.out.print("masukkan : ");
+        a = input.nextInt();
+        System.out.print("masukkan : ");
+        b = input.nextInt();
+        a++;
+        b--;
+        System.out.println("Increment :" + a);
+        System.out.println("Decrement :" + b);
+        
+    }
+}
