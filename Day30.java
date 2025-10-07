758fec3c025743cbcab7ad79cbc2b28527ba6731
muh. hasyim <h13880134@gmail.com>
2025-10-07 23:58:40 +0800

    Day30.java

---
+++ Day30.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Hasimd0225303 {
+    
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Angka : ");
+        int a = input.nextInt();
+        System.out.print("Angka : ");
+        int b = input.nextInt();
+               
+        System.out.println("\nPerbandingan : ");
+        System.out.println(a + " <= " +b+" = "+(a<=b));
+        System.out.println(a + " >= " +b+" = "+(a>=b));
+        
+    }
+    
+}
