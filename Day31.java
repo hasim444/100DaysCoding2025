3afbb2bdb2e48f3b4b9650438623c0d217fc0af2
muh. hasyim <h13880134@gmail.com>
2025-10-08 22:03:56 +0800

    Day31.java

---
+++ Day31.java
@@ -0,0 +1,15 @@
+public class Hasimd0225303 {
+    
+    public static void main(String[] args) {
+       
+        int a = 5;
+        int b = 10;
+        int c = 5;
+        System.out.println("menggunakan operatot &&");
+        System.out.println("a > b && a > c: " + (a > b && a > c));
+        System.out.println("a > b && a < c: " + (a > b && a < c));
+        System.out.println("a > b && a != c: " + (a > b && a != c));
+    
+    }
+    
+}
