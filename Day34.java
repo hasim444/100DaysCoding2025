08ed4babbfd1d512e7861f8bb1e1375e8ec17916
muh. hasyim <h13880134@gmail.com>
2025-10-11 23:10:10 +0800

    Day34.java

---
+++ Day34.java
@@ -0,0 +1,13 @@
+System.out.print("nilai :");
+        int a = input.nextInt();
+        System.out.print("nilai :");
+        int b = input.nextInt();
+        if (a >=55 && b >=40){
+            System.out.print("anda bodo");
+        } else if (a >=40 || b>=35){
+            System.out.print("anda pintar");
+    }else{
+        System.out.print("silahkan belajar lagi");
+    }
+    }
+}
